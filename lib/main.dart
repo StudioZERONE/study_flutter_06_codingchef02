d86230e8c2f6a8116c6840939d582ab2b3e0d90c
ZERONE <zerone012@gmail.com>
2023-04-03 10:30:15 +0900

    로그인 화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +26,74 @@ class LogIn extends StatelessWidget {
         title: Text('Log in'),
         backgroundColor: Colors.redAccent,
         centerTitle: true,
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: (){}
-        ),
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
         actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){}
-          )
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 50),
+            ),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
+            ),
+            Form(
+              child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(40),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "dice"',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter Password',
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ButtonTheme(
+                          minWidth: 100,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
